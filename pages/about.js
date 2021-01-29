2e8f3b0153407d8168e353bedee7e8414930ff6f
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-01-29 12:15:19 +0100

    creating nextjs learning pages and routes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>fwefnlwefnlwefnklwef-,cle welefnlwf</p>
+      <p>fwefnlwefnlwefnklwef-,cle welefnlwf</p>
+    </div>
+  );
+}
---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Ninjas() {
+  return (
+    <div>
+      <h1> All Ninjas</h1>
+    </div>
+  );
+}
