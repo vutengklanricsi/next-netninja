543730c050e0159c6dc759a8f25631d3027cf840
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-01-29 14:25:28 +0100

    adding layout to pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,15 +7,17 @@ export default function Navbar() {
       <div className="logo">
         <h1>Ninja list</h1>
       </div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/ninjas">
-        <a>Ninja listing</a>
-      </Link>
+      <div className="navbar">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/ninjas">
+          <a>Ninja listing</a>
+        </Link>
+      </div>
     </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>HomePage</h1>
       <p>dormire cem cwe fjwefnlfklwfennwfeklwefő wel</p>
       <p>dormire cem cwe fjwefnlfklwfennwfeklwefő wel</p>
       <Link href="./ninjas"><a>See Ninja Listing</a></Link>
-      <Footer />
     </div>
   );
 }
