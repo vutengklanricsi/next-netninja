ff4ff003c6d5d4ecea7e65799665599ebe2d61dd
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-02-25 16:05:37 +0100

    fetching data from jsonplacholder

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,31 @@
 import React from "react";
+import styles from "../../styles/Ninjas.module.css"
 
-export default function Ninjas() {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+export default function Ninjas({ ninjas }) {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas
+        ? ninjas.map((ninja) => {
+            return (
+              <div key={ninja.id}>
+                <a className={styles.single}>
+                  <h3>{ninja.name}</h3>
+                </a>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 }
